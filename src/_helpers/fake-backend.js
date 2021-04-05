f42980f856aa76771facfe0017f41311192cc589
DESKTOP-PRG3M7N\ToanNCK <toannck32@wru.vn>
2021-04-05 15:50:00 +0700

    Updtate

---
+++ src/_helpers/fake-backend.js
@@ -0,0 +1,49 @@
+export function configureFakeBackend() {
+    let users = [{ id: 1, username: 'admin@gmail.com', password: 'Admin@123', firstName: 'Nguyễn Công Khánh ', lastName: 'Toàn' }];
+    let realFetch = window.fetch;
+    window.fetch = function (url, opts) {
+        const isLoggedIn = opts.headers['Authorization'] === 'Bearer fake-jwt-token';
+
+        return new Promise((resolve, reject) => {
+            // wrap in timeout to simulate server api call
+            setTimeout(() => {
+                // authenticate - public
+                if (url.endsWith('/users/authenticate') && opts.method === 'POST') {
+                    const params = JSON.parse(opts.body);
+                    const user = users.find(x => x.username === params.username && x.password === params.password);
+                    if (!user) return error('Tên đăng nhập hoặc mật khẩu không đúng');
+                    return ok({
+                        id: user.id,
+                        username: user.username,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        token: 'fake-jwt-token'
+                    });
+                }
+
+                // get users - secure
+                if (url.endsWith('/users') && opts.method === 'GET') {
+                    if (!isLoggedIn) return unauthorised();
+                    return ok(users);
+                }
+
+                // pass through any requests not handled above
+                realFetch(url, opts).then(response => resolve(response));
+
+                // private helper functions
+
+                function ok(body) {
+                    resolve({ ok: true, text: () => Promise.resolve(JSON.stringify(body)) })
+                }
+
+                function unauthorised() {
+                    resolve({ status: 401, text: () => Promise.resolve(JSON.stringify({ message: 'Unauthorised' })) })
+                }
+
+                function error(message) {
+                    resolve({ status: 400, text: () => Promise.resolve(JSON.stringify({ message })) })
+                }
+            }, 500);
+        });
+    }
+}
