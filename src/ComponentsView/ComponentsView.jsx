435df20764628cf9280f52bc08cc95a8f07c90ac
DESKTOP-PRG3M7N\ToanNCK <toannck32@wru.vn>
2021-04-05 18:29:18 +0700

    aaa

--- src/ComponentsView/ComponentsView.jsx
+++ src/ComponentsView/ComponentsView.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Alert } from 'react-bootstrap';
 import { userService, authenticationService } from '@/_services';
 
 class ComponentsView extends React.Component {
@@ -8,7 +8,8 @@ class ComponentsView extends React.Component {
 
         this.state = {
             currentUser: authenticationService.currentUserValue,
-            users: null
+            users: null,
+            alerts: ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark',]
         };
     }
 
@@ -18,17 +19,20 @@ class ComponentsView extends React.Component {
 
     render() {
         const { currentUser, users } = this.state;
+        const data = this.state.alerts;
         return (
             <div>
-                <h1 className="col-md-12">{currentUser.firstName} {currentUser.lastName}!</h1>
-                <h3>Người dùng:</h3>
-                {users &&
-                    <ul>
-                        {users.map(user =>
-                            <li key={user.id}>{user.firstName} {user.lastName}</li>
-                        )}
-                    </ul>
+                <h3>Người dùng: {currentUser.firstName} {currentUser.lastName}</h3>
+                {
+                    data &&
+                    data.map((variant, idx) => (
+                        <Alert key={idx} variant={variant}>
+                            This is a {variant} alert—check it out!
+                        </Alert>
+                    ))
+
                 }
+
             </div>
         );
     }
