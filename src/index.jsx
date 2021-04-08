dcc257d43f1b705c30e9ac42716f4e1ce32578ee
DESKTOP-PRG3M7N\ToanNCK <toannck32@wru.vn>
2021-04-08 18:47:10 +0700

    comit

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { App } from './App';
+import './Assets/';
 
 // setup fake backend
 import { configureFakeBackend } from './_helpers';
