5aea644ce54df528feba5ef6ebd23ab68d8f00f7
toan nguyen <toannck32@wru.vn>
2021-10-21 21:20:00 +0700

    update

---
+++ aspnet-core-3-jwt-authentication-api/Migrations/20211021141400_AddUserDefault.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class AddUserDefault : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [dbo].[User] ([FirstName] ,[LastName] ,[Username] ,[Password]) VALUES (N'Nguyễn Công Khánh', N'Toàn', 'admin@gmail.com', 'Admin@123') " +
+                                 "INSERT INTO [dbo].[User] ([FirstName] ,[LastName] ,[Username] ,[Password]) VALUES (N'Test', N'thôi mà :)', 'test@gmail.com', 'Test@123')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
